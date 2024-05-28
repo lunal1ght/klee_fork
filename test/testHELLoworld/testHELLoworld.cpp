090d13d3bbd0199a3682778bb37995f11316d50c
lunal1ght <alltakethat228@gmail.com>
2024-05-29 02:35:15 +0300

    i want to die 7

--- test/testHELLoworld/testHELLoworld.cpp
+++ test/testHELLoworld/testHELLoworld.cpp
@@ -1,15 +1,16 @@
+// REQUIRES: not-msan
+// Disabling msan because it times out on CI
 // REQUIRES: libcxx
 // REQUIRES: uclibc
-// RUN: %clangxx -emit-llvm %O0opt -std=c++11 %libcxx_includes -nostdinc++ -g -c %s -o %t.bc
+// RUN: %clangxx %s -emit-llvm %O0opt -c -std=c++11 %libcxx_includes -g -nostdinc++ -o %t1.bc
 // RUN: rm -rf %t.klee-out
 // RUN: %klee --output-dir=%t.klee-out --libc=uclibc --libcxx %t1.bc 2>&1 | FileCheck %s
 
 #include "klee/klee.h"
 #include <iostream>
-using namespace std;
 
 int main() 
 { 
-    cout << "Hello, world!" << endl;
+    std::cout << "Hello, world!" << std::endl;
     return 0; 
 }
