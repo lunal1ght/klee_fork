e44e3155a7f9f2ad6eb2d0d48f3c8a4a30b8575d
lunal1ght <alltakethat228@gmail.com>
2024-05-29 02:54:06 +0300

    i want to die 8

--- test/testHELLoworld/testHELLoworld.cpp
+++ test/testHELLoworld/testHELLoworld.cpp
@@ -2,7 +2,7 @@
 // Disabling msan because it times out on CI
 // REQUIRES: libcxx
 // REQUIRES: uclibc
-// RUN: %clangxx %s -emit-llvm %O0opt -c -std=c++11 %libcxx_includes -g -nostdinc++ -o %t1.bc
+// RUN: %clangxx %s -emit-llvm -O0 -Xclang -disable-O0-optnone -c -std=c++11 %libcxx_includes -g -nostdinc++ -o %t1.bc
 // RUN: rm -rf %t.klee-out
 // RUN: %klee --output-dir=%t.klee-out --libc=uclibc --libcxx %t1.bc 2>&1 | FileCheck %s
 
@@ -14,3 +14,5 @@ int main()
     std::cout << "Hello, world!" << std::endl;
     return 0; 
 }
+
+// CHECK: Hello, world!
